4bcea0f18760c811d9373cc442d9379a7518cf55
brett <brett@bmb.io>
2021-01-25 23:01:46 +0000

    comment parse.py

--- prots/parse.py
+++ prots/parse.py
@@ -1,5 +1,8 @@
 #!/usr/bin/env python2
 
+# This is to see what the unmarshalled dict looks like
+# p4 -G info | ./parse.py
+
 import sys, marshal
 
 try:
@@ -8,9 +11,9 @@ try:
         num=num+1
         print '\n--%d--' % num
         dict =  marshal.load(sys.stdin)
-        for key in dict.keys(): 
+        for key in dict.keys():
             print "%s: %s" % (key,dict[key])
 
-except EOFError: 
-    pass 
+except EOFError:
+    pass
 
