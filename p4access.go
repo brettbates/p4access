51c7bf0bdbcf374a8f07b99cddf4733147becd0a
brett <brett@bmb.io>
2021-01-26 08:36:49 +0000

    basic cmd line template

--- p4access.go
+++ p4access.go
@@ -1,14 +1,41 @@
 package main
 
 import (
-	"fmt"
-
-	"github.com/brettbates/p4access/prots"
+	"html/template"
+	"io/ioutil"
+	"log"
+	"os"
 )
 
+// Info is the path and owners of a group
+type Info struct {
+	Access string
+	Path   string
+	Owners []string
+}
+
+// Groups maps Group names to their path/owners
+type Groups map[string]Info
+
+func output() {
+	tmp, err := ioutil.ReadFile("response.txt")
+	if err != nil {
+		log.Fatalln("Failed to find answer.txt template")
+	}
+	t := template.Must(template.New("response").Parse(string(tmp)))
+	out := struct{ Groups Groups }{Groups: Groups{
+		"Test group": Info{
+			"Read",
+			"Test Path",
+			[]string{"owner 1", "owner 2"},
+		},
+	}}
+	err = t.Execute(os.Stdout, out)
+	if err != nil {
+		log.Fatalf("Failed to execute template\n%v", err)
+	}
+}
+
 func main() {
-	p4c := prots.NewP4C()
-	res, err := prots.Protections(p4c, "//depot/...")
-	fmt.Println(err)
-	fmt.Println(res)
+	output()
 }
